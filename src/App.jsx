8168e555257148a8c692f5a601f01187a7c6de95
mdismaildev <mrismaildev1@gmail.com>
2026-07-18 18:37:51 +0600

    added Navbar and Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-
-import './App.css'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
-
-
+  const isOwnerPath = useLocation().pathname.includes('owner');
   return (
     <>
+      <div className="">
+        {!isOwnerPath && <Navbar />}
 
+        <div className="min-h-[70vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:32 text-white bg-[url('/src/assets/heroImage.png')] bg-no-repeat bg-cover bg-center h-screen">
+      Hero
+    </div>
+  );
+};
+
+export default Hero;
